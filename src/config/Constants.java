047ba86ddb723b5ce0e2682190eeac66893dab21
Arash <ash.baghernejad.23@sharif.edu>
2026-07-11 02:42:34 +0330

    Creating Explorer class.

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -11,4 +11,8 @@ public final class Constants {
     public static final int MOUNTAIN_MOVEMENT_COST = 4;
 
     public static final int MEADOW_MOVEMENT_COST = 1;
+
+    public static final int EXPLORER_MAX_AP = 4;
+
+    public static final int EXPLORER_VISION_RADIUS = 3;
 }
---
+++ src/model/unit/Explorer.java
@@ -0,0 +1,10 @@
+package model.unit;
+
+import config.Constants;
+import model.hex.Hex;
+
+public class Explorer extends Unit {
+    public Explorer(Hex startingHex) {
+        super(startingHex, Constants.EXPLORER_MAX_AP, Constants.EXPLORER_VISION_RADIUS);
+    }
+}
