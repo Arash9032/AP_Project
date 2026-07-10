b1da7dc9bbe1a4c659d46f12d8bbf405d76f76f1
Arash <ash.baghernejad.23@sharif.edu>
2026-07-11 02:59:47 +0330

    Changing the max APs so that every unit can move certainly.

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -2,27 +2,20 @@ package config;
 
 public final class Constants {
 
-    private Constants(){}
+    private Constants() {}
 
     public static final int FOREST_MOVEMENT_COST = 2;
-
     public static final int PLAIN_MOVEMENT_COST = 1;
-
     public static final int MOUNTAIN_MOVEMENT_COST = 4;
-
     public static final int MEADOW_MOVEMENT_COST = 1;
 
-    public static final int EXPLORER_MAX_AP = 4;
-
+    public static final int EXPLORER_MAX_AP = 6;
     public static final int EXPLORER_VISION_RADIUS = 3;
 
-    public static final int BUILDER_MAX_AP = 2;
-
+    public static final int BUILDER_MAX_AP = 4;
     public static final int BUILDER_VISION_RADIUS = 2;
-
     public static final int BUILDER_INITIAL_CHARGES = 3;
 
-    public static final int WORKER_MAX_AP = 2;
-
+    public static final int WORKER_MAX_AP = 4;
     public static final int WORKER_VISION_RADIUS = 1;
 }
