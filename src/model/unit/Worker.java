c3ffcdb8d52b3cba0c5fe1ac755a4b82e8d4ec0d
Arash <ash.baghernejad.23@sharif.edu>
2026-07-11 02:50:06 +0330

    Creating Worker class.

---
+++ src/model/unit/Worker.java
@@ -0,0 +1,21 @@
+package model.unit;
+
+import config.Constants;
+import model.hex.Hex;
+
+public class Worker extends Unit {
+    private boolean isStationed;
+
+    public Worker(Hex startingHex) {
+        super(startingHex, Constants.WORKER_MAX_AP, Constants.WORKER_VISION_RADIUS);
+        this.isStationed = false;
+    }
+
+    public void setStationed(boolean stationed) {
+        this.isStationed = stationed;
+    }
+
+    public boolean isStationed() {
+        return isStationed;
+    }
+}
