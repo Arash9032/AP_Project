d524f8aa408378356e26a4f42e9ea3bd49528fdd
Arash <ash.baghernejad.23@sharif.edu>
2026-07-26 13:45:59 +0330

    Adding HP field to the Building abstract class.

--- src/model/map/building/Building.java
+++ src/model/map/building/Building.java
@@ -11,6 +11,7 @@ public abstract class Building {
     protected final Map<ResourceType, Integer> constructionCost;
     protected final Map<ResourceType, Integer> upkeepCost;
     protected int consecutiveUnpaidUpkeep;
+    protected int HP = 100;
 
     public Building(Hex location, int constructionAPCost, Map<ResourceType, Integer> constructionCost, Map<ResourceType, Integer> upkeepCost) {
         this.location = location;
@@ -46,5 +47,21 @@ public abstract class Building {
 
     public Map<ResourceType, Integer> getUpkeepCost() {
         return upkeepCost;
+    }
+
+    public int getConsecutiveUnpaidUpkeep() {
+        return consecutiveUnpaidUpkeep;
+    }
+
+    public void setConsecutiveUnpaidUpkeep(int consecutiveUnpaidUpkeep) {
+        this.consecutiveUnpaidUpkeep = consecutiveUnpaidUpkeep;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
     }
 }
