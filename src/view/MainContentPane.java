eb38967cb480b1122176cf34cba961a576386545
Arash <ash.baghernejad.23@sharif.edu>
2026-08-01 12:53:46 +0330

    Creating MainContentPane class.

---
+++ src/view/MainContentPane.java
@@ -0,0 +1,25 @@
+package view;
+
+import view.panel.PanelType;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainContentPane extends JPanel {
+    private final CardLayout layout;
+    public MainContentPane() {
+        layout = new CardLayout();
+        setLayout(layout);
+        addPanels();
+    }
+
+    private void addPanels(){
+        for(PanelType panelType : PanelType.values()){
+            add(panelType.createPage() , panelType.name());
+        }
+    }
+
+    public void showPanel(PanelType panelType){
+        layout.show(this , panelType.name());
+    }
+}
