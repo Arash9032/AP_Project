53b030e1cb9b262ddf79fe6b57050ccdb6cccb26
Arash <ash.baghernejad.23@sharif.edu>
2026-08-06 13:35:04 +0330

    Adding mainContentPain as a field to the MainFrame class.

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -6,12 +6,22 @@ import controller.GameEngine;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    private MainContentPane mainContentPane;
     public MainFrame(GameEngine engine) {
         super("Hex Land: Rise of Tribes");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);
         setLocationRelativeTo(null);
-        setContentPane(new MainContentPane(engine));
+        mainContentPane = new MainContentPane(engine);
+        setContentPane(mainContentPane);
         setVisible(true);
+    }
+
+    public MainContentPane getMainContentPane() {
+        return mainContentPane;
+    }
+
+    public void setMainContentPane(MainContentPane mainContentPane) {
+        this.mainContentPane = mainContentPane;
     }
 }
