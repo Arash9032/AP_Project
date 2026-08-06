be543a2791b6a66b0ab4269ee44479054bd52a66
Arash <ash.baghernejad.23@sharif.edu>
2026-08-06 14:06:43 +0330

    Creating ZoomListener class.

---
+++ src/controller/ZoomListener.java
@@ -0,0 +1,19 @@
+package controller;
+
+import view.panel.GamePanel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseWheelEvent;
+
+public class ZoomListener extends MouseAdapter {
+    private GamePanel gamePanel;
+
+    public ZoomListener(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        gamePanel.applyZoom(e.getPreciseWheelRotation());
+    }
+}
