9a3f5b6aa15f00ecc6518e83fbcff4bc4900c29f
Arash <ash.baghernejad.23@sharif.edu>
2026-08-08 16:24:35 +0330

    Creating TurnManager class.

---
+++ src/controller/TurnManager.java
@@ -0,0 +1,32 @@
+package controller;
+
+import controller.eventbus.EventBus;
+import controller.eventbus.TurnEndedEvent;
+import model.GameState;
+import model.unit.Unit;
+
+public class TurnManager {
+    private GameState gameState;
+    private int currentTurn;
+
+    public TurnManager(GameState gameState) {
+        this.gameState = gameState;
+        this.currentTurn = 1;
+    }
+
+    public void endTurn() {
+        currentTurn++;
+        resetUnitsAP();
+        EventBus.getInstance().publish(new TurnEndedEvent(currentTurn));
+    }
+
+    private void resetUnitsAP() {
+        for (Unit unit : gameState.getUnits()) {
+            unit.resetAP();
+        }
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+}
