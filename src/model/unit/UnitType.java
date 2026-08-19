360ba05444c99d62b6bfd586ca5a9d39a9f2670e
Arash <ash.baghernejad.23@sharif.edu>
2026-08-19 09:44:11 +0330

    Replacing the abstract createUnit() method with a constructor field in UnitType.

--- src/model/unit/UnitType.java
+++ src/model/unit/UnitType.java
@@ -6,55 +6,42 @@ import model.map.building.townhall.InventoryResource;
 import model.map.hex.Hex;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public enum UnitType {
 
     WORKER(
             Constants.WORKER_PRODUCTION_TURN_COST,
-            new CostBuilder().add(InventoryResource.FOOD, Constants.WORKER_FOOD_COST).build()
-    ) {
-        @Override
-        public Unit createUnit(Hex location) {
-            return new Worker(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.FOOD, Constants.WORKER_FOOD_COST).build(),
+            Worker::new
+    ),
     BUILDER(
             Constants.BUILDER_PRODUCTION_TURN_COST,
-            new CostBuilder().add(InventoryResource.FOOD, Constants.BUILDER_FOOD_COST).build()
-    ) {
-        @Override
-        public Unit createUnit(Hex location) {
-            return new Builder(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.FOOD, Constants.BUILDER_FOOD_COST).build(),
+            Builder::new
+    ),
     EXPLORER(
             Constants.EXPLORER_PRODUCTION_TURN_COST,
-            new CostBuilder().add(InventoryResource.FOOD, Constants.EXPLORER_FOOD_COST).build()
-    ) {
-        @Override
-        public Unit createUnit(Hex location) {
-            return new Explorer(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.FOOD, Constants.EXPLORER_FOOD_COST).build(),
+            Explorer::new
+    ),
     BORDER_EXPANDER(
             Constants.BORDER_EXPANDER_PRODUCTION_TURN_COST,
             new CostBuilder()
                     .add(InventoryResource.FOOD, Constants.BORDER_EXPANDER_FOOD_COST)
                     .add(InventoryResource.WOOD, Constants.BORDER_EXPANDER_WOOD_COST)
-                    .build()
-    ) {
-        @Override
-        public Unit createUnit(Hex location) {
-            return new BorderExpander(location);
-        }
-    };
+                    .build(),
+            BorderExpander::new
+    );
 
     private final int productionTurnCost;
     private final Map<InventoryResource, Integer> productionCost;
+    private final Function<Hex , Unit> constructor;
 
-    UnitType(int productionTurnCost, Map<InventoryResource, Integer> productionCost) {
+    UnitType(int productionTurnCost, Map<InventoryResource, Integer> productionCost, Function<Hex, Unit> constructor) {
         this.productionTurnCost = productionTurnCost;
         this.productionCost = productionCost;
+        this.constructor = constructor;
     }
 
     public int getProductionTurnCost() {
@@ -65,5 +52,7 @@ public enum UnitType {
         return productionCost;
     }
 
-    public abstract Unit createUnit(Hex location);
+    public Unit createUnit(Hex location){
+        return constructor.apply(location);
+    }
 }
