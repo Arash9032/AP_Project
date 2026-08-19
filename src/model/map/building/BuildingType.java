9d9b12ca8e0e6059ca8439925794b328389888d1
Arash <ash.baghernejad.23@sharif.edu>
2026-08-19 09:40:46 +0330

    Replacing the createInstance() method with a constructor field in BuildingType.

--- src/model/map/building/BuildingType.java
+++ src/model/map/building/BuildingType.java
@@ -6,63 +6,43 @@ import model.map.building.production.*;
 import model.map.building.townhall.InventoryResource;
 import model.map.building.townhall.TownHall;
 import model.map.hex.Hex;
-import model.map.hex.Point;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.function.Function;
 
 public enum BuildingType {
 
     LUMBER_MILL(
             Constants.LUMBER_MILL_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.LUMBER_MILL_WOOD_COST).build(),
-            new CostBuilder().add(InventoryResource.WOOD , Constants.LUMBER_MILL_WOOD_UPKEEP).build()
-            ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new LumberMill(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.WOOD , Constants.LUMBER_MILL_WOOD_UPKEEP).build(),
+            LumberMill::new
+            ),
     FARM(
             Constants.FARM_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.FARM_WOOD_COST).build(),
-            new CostBuilder().add(InventoryResource.WOOD , Constants.FARM_WOOD_UPKEEP).build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new Farm(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.WOOD , Constants.FARM_WOOD_UPKEEP).build(),
+            Farm::new
+    ),
     STONE_MINE(
             Constants.STONE_MINE_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.STONE_MINE_WOOD_COST).build(),
-            new CostBuilder().add(InventoryResource.WOOD , Constants.STONE_MINE_WOOD_UPKEEP).build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new StoneMine(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.WOOD , Constants.STONE_MINE_WOOD_UPKEEP).build(),
+            StoneMine::new
+    ),
     IRON_MINE(
             Constants.IRON_MINE_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.IRON_MINE_WOOD_COST).build(),
-            new CostBuilder().add(InventoryResource.WOOD , Constants.IRON_MINE_WOOD_UPKEEP).build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new IronMine(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.WOOD , Constants.IRON_MINE_WOOD_UPKEEP).build(),
+            IronMine::new
+    ),
     STABLE(
             Constants.STABLE_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.STABLE_WOOD_COST).build(),
-            new CostBuilder().add(InventoryResource.WOOD , Constants.STABLE_WOOD_UPKEEP).build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new Stable(location);
-        }
-    },
+            new CostBuilder().add(InventoryResource.WOOD , Constants.STABLE_WOOD_UPKEEP).build(),
+            Stable::new
+    ),
     VILLAGE(
             Constants.VILLAGE_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.VILLAGE_WOOD_COST)
@@ -72,13 +52,9 @@ public enum BuildingType {
             new CostBuilder().add(InventoryResource.WOOD , Constants.VILLAGE_WOOD_UPKEEP)
                     .add(InventoryResource.STONE , Constants.VILLAGE_STONE_UPKEEP)
                     .add(InventoryResource.IRON , Constants.VILLAGE_IRON_UPKEEP)
-                    .build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new Village(location);
-        }
-    },
+                    .build(),
+            Village::new
+    ),
     TOWN(
             Constants.TOWN_CONSTRUCTION_AP_COST,
             new CostBuilder().add(InventoryResource.WOOD , Constants.TOWN_WOOD_COST)
@@ -88,47 +64,39 @@ public enum BuildingType {
             new CostBuilder().add(InventoryResource.WOOD , Constants.TOWN_WOOD_UPKEEP)
                     .add(InventoryResource.STONE , Constants.TOWN_STONE_UPKEEP)
                     .add(InventoryResource.IRON , Constants.TOWN_IRON_UPKEEP)
-                    .build()
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            return new Town(location);
-        }
-    },
+                    .build(),
+            Town::new
+    ),
     TOWN_HALL(
             0,
             Collections.emptyMap(),
-            Collections.emptyMap()
-
-    ) {
-        @Override
-        public Building createInstance(Hex location) {
-            if(!location.getCoordinate().equals(new Point(0,0))) throw new UnsupportedOperationException("Town hall cannot be built on any hex other than (0,0)");
-            return new TownHall(location);
-        }
-    };
+            Collections.emptyMap(),
+            TownHall::new
+    );
 
     private final int constructionApCost;
     private final Map<InventoryResource , Integer> constructionCost;
     private final Map<InventoryResource , Integer> upkeepCost;
+    private final Function<Hex , Building> constructor;
+
     BuildingType(
             int constructionApCost,
             Map<InventoryResource, Integer> constructionCost,
-            Map<InventoryResource, Integer> upkeepCost
+            Map<InventoryResource, Integer> upkeepCost,
+            Function<Hex , Building> constructor
     ) {
         this.constructionApCost = constructionApCost;
         this.constructionCost = constructionCost;
         this.upkeepCost = upkeepCost;
+        this.constructor = constructor;
     }
 
     public int getConstructionApCost() {
         return constructionApCost;
     }
 
-    protected abstract Building createInstance(Hex location);
-
     public Building createBuilding(Hex location){
-        Building building = createInstance(location);
+        Building building = constructor.apply(location);
         location.setBuilding(building);
         return building;
     }
